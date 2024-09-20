b2041b345bef5c8b7f5b652e6c19e9264d52bea7
William Jones <williamseejones793@gmail.com>
2024-09-20 10:46:32 -0500

    added product and treatment controllers, filled out product controller

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,12 @@
 class Product < ApplicationRecord
+  validates :name, presence: true
+  validates :cost, presence: true
+  validates :square_feet, presence: true
+  validates :group, presence: true
+
+  attr_accessor :cost_per_square_foot
+
+  def cost_per_square_foot
+    @cost_per_square_foot = (self.cost/self.square_feet).round(4)
+  end
 end
